01acf63546667fd4b23ef55efd03d73687ab8250
devyaz <mtunthama@gmail.com>
2021-07-02 01:46:11 +0200

    Update view.ts
    tidying up the code

--- app/controllers/view.ts
+++ app/controllers/view.ts
@@ -1,14 +1,9 @@
 import {Observable,EventData} from "@nativescript/core/data/observable"
 import { ObservableArray} from "@nativescript/core/data/observable-array";
-var utils = require("@nativescript/core/utils/utils");
+//var utils = require("@nativescript/core/utils/utils");
 
 export const model = new Observable()
-/*
-model.background=function(){
-    services.setupAlarm(utils.ad.getApplicationContext());
-    console.log(" trying to set alarm!!");
-}*/
+
 
  
 
-//exports.defaults  = model;
